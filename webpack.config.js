89eb5ee2156f1b04541ed3ef6a4db60bb9541895
RXstoinkboy <erykludwin@gmail.com>
2018-12-06 22:11:06 +0100

    webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 module.exports = {
   entry: {
-    index: './src/js/index.js',
+    index: './src/js/index.mjs',
   },
   output: {
     path: `${__dirname}/dist/js`,
-    filename: 'legacy-[name].js',
+    filename: 'legacy-[name].mjs',
   },
   watch: true,
   mode: 'development', // ta opcja zostanie pominięta jeżeli użyjemy npm run build
   devtool: 'source-map',
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['env', {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
               targets: {
                 browsers: ['> 1%'],
               },
-            }]],
-          },
+            }],
+          ],
         },
       },
-    ],
+    }],
   },
 };
